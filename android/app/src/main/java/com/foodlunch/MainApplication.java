f7b185068198071fe6eefac9ff4703d2d6791a3a
Saharak <Saharakmanoo@outlook.com>
2020-02-22 10:54:00 +0700

    [Fixed] code

--- android/app/src/main/java/com/foodlunch/MainApplication.java
+++ android/app/src/main/java/com/foodlunch/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.codemotionapps.reactnativedarkmode.DarkModePackage;
 import com.swmansion.rnscreens.RNScreensPackage;
 import org.reactnative.maskedview.RNCMaskedViewPackage;
 import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
